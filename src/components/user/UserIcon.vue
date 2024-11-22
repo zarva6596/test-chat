206e0112d7c81d7f0b742511ac8bf61f7217bbaa
zarva6596 <zarva6596@gmail.com>
2024-11-23 00:42:54 +0200

    feat: create user icon component

---
+++ src/components/user/UserIcon.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const SIZES = {
+  SMALL: 'small',
+  MEDIUM: 'medium',
+  LARGE: 'large'
+} as const;
+
+const { firstName, lastName, size } = defineProps<{
+    firstName: string
+    lastName: string
+    size?: typeof SIZES[keyof typeof SIZES]
+}>()
+
+const initials = computed(() => `${firstName.at(0)}${lastName.at(0)}`.toUpperCase())
+
+const iconRadius = computed(() => {
+    switch(size) {
+        case SIZES.SMALL:
+            return '1.5rem'
+        case SIZES.LARGE:
+            return '2.5rem'
+        case SIZES.MEDIUM:
+        default:
+            return '2rem'    
+    }
+})
+
+const fontSize = computed(() => {
+    switch(size) {
+        case SIZES.SMALL:
+            return '.75rem'
+        case SIZES.LARGE:
+            return '1.5rem'
+        case SIZES.MEDIUM:
+        default:
+            return '1rem'    
+    }
+})
+</script>
+
+<template>
+    <div
+        class="flex items-center justify-center rounded-full border border-white text-white bg-black"
+        :style="{
+            width: iconRadius,
+            height: iconRadius
+        }"
+    >
+        <span :style="{ fontSize }">{{ initials }}</span>
+    </div>
+</template>
